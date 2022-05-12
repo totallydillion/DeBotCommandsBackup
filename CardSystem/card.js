317bd4456251897b35785874f0a090a5fa97999e
totallydillion <95979395+totallydillion@users.noreply.github.com>
2022-05-11 23:49:06 -0400

    updated card.js
    bug fixes

--- CardSystem/card.js
+++ CardSystem/card.js
@@ -13,10 +13,17 @@ const ClaimBTN = new MessageActionRow().addComponents(
     
     module.exports = {
         name:'card',
-        description:`Get Anime Cards to collect, trade, and more! [WIP]`,
-        category: 'Card',
+        description:`Get An Anime Card that you can claim!`,
+        category: 'Cc',
         async execute(message, args){
+            /* on command ran */
+            
+            
             anime.get_random_character().then(result => {
+                /* get random anime character from npm */
+                
+                
+                /* show the user */
                 message.channel.send({embeds:[{
                     title: `${result.name} | ${result.gender}`,
                     description: result.desc,
@@ -25,24 +32,37 @@ const ClaimBTN = new MessageActionRow().addComponents(
                         url: result.character_image
                     }
                 }], components: [ClaimBTN]}).then(function(animeMessage){
+                    
+                    /* a filter for the "Claim" button */
                     const filter = (interaction) => {
                         if(interaction.user.id == message.author.id ) return true;
                         else interaction.reply({ content: 'This is not you character to claim!', ephemeral: true });
                     }
+                    
+                    /* create an event listener */
                     const collector = animeMessage.createMessageComponentCollector({filter, time: 120000})
                     
                     collector.on('collect', async (interaction) => {
+                        /* on collect, get the button pressed */ 
                         let choice = interaction.customId;
                         
                         
                         /* on claiming waifu */
                         if(choice == "Claim"){
+                            
+                            /* Call Claiming Card */
                             ClaimCard(result, message.author)
                             
+                            /* Update the interaction, so it does not give "Interaction failed" */
                             interaction.deferUpdate();
-
-                            message.reply(`You Have Claimed: **${result.name}**`)
+                            
+                            /* Let the user know that they have claimed the waifu */
+                            message.reply(`You Have Claimed: **${result.name}**\nTo view your cards, type **=cardinv**`)
+                            
+                            /* stop the listener */
                             collector.stop();
+                            
+                            /* return */
                             return;
                         }
                     })
@@ -52,55 +72,98 @@ const ClaimBTN = new MessageActionRow().addComponents(
     }
     
     function ClaimCard(card, user){
+        
+        /* on claiming card */
+        
+        
+        /* read json */
         fs.readFile('./commands/PerServer/UserCards.json', 'utf-8', function(err, data){
+            
+            /* if error, throw it like it's a baseball. */
             if(err) throw err;
             
+            /* get all of the users */
             var users = JSON.parse(data);
             
-            // if(!users.includes(user.id)){
+            /* has the user been found ? */
+            let isUserFound = false;
             
+            /* map out all of the users, this is so we are not using a for statement for every user */
             users.map((thisUser) => {
+                
+                /* if the map finds an ID that is the same as the user's id */
                 if(thisUser.id == user.id){
-                    thisUser.CardsOwned.push(card);
+                    
+                    /* user has been found */ 
+                    isUserFound = true;
+                    
+                    /* set up the card entry, this is what is being saved */
+                    let cardEntry = {
+                        name: card.name,
+                        gender: card.gender,
+                        character_image: card.character_image.toString()
+                    }
+                    
+                    /* push the card entry into the user's owned cards */
+                    thisUser.CardsOwned.push(cardEntry);
+                    
+                    /* saving */
                     fs.writeFile('./commands/PerServer/UserCards.json', JSON.stringify(users), 'utf-8', function(err){
                         if(err) throw err;
                         else return;
                     })
+                    
+                    /* returning */
                     return;
                 }
             })
             
+            /* map the users again */
             users.map(function(thisUser){
-                switch(thisUser.id){    
+                
+                /* this time, we are finding a specific user */
+                switch(thisUser.id){ 
                     
+                    /* find the user's id, if it is found, break */
                     case user.id:
                     break;
                     
                     
+                    /* if it has not been found, aka: this is the first card being collected */
                     default:
+                    
+                    /* double check, and making sure it was not found */
+                    if(isUserFound) break;
+
+                    /* set up a default entry, this is what is being saved and searched for when inventory is called */
                     var defaultEntry = {
                         "id": user.id,
                         "CardsOwned": [],
                         "CardsWishlist": [],
                     }
+
+                    /* get the card entry, this is what is being saved */
+                    let cardEntry = {
+                        name: card.name,
+                        gender: card.gender,
+                        character_image: card.character_image.toString()
+                    }
                     
-                    defaultEntry.CardsOwned.push(card);
+                    /* in the user's default entry, push the card */
+                    defaultEntry.CardsOwned.push(cardEntry);
                     
+                    /* in the json, push the user and default entry */
                     users.push(defaultEntry);
                     
+                    /* save */
                     fs.writeFile('./commands/PerServer/UserCards.json', JSON.stringify(users), 'utf-8', function(err){
                         if(err) throw err;
                         else return;
                     })
-                    
+                    /* break, we are done here. */
                     break;
                     
                 }
             })
-            
-            
-            
-            
-            // }
         })
     }
